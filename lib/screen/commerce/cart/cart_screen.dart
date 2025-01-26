94a286685dcfcf97afde69ecce3a846bd07c263f
인삼 <108407114+iaminsam@users.noreply.github.com>
2025-01-26 21:57:48 +0900

    Add files via upload

---
+++ lib/screen/commerce/cart/cart_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:online_lecture_smwu/screen/commerce/cart/cart_view_model.dart';
+import 'package:online_lecture_smwu/screen/commerce/home/widgets/product_grid.dart';
+
+class CartScreen extends StatefulWidget {
+  const CartScreen({super.key});
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  CartViewModel vm = CartViewModel.instance;
+  VoidCallback? listener;
+
+  @override
+  void initState() {
+    listener = () {
+      setState(() {});
+    };
+    vm.addListener(listener!);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    vm.removeListener(listener!);
+    listener = null;
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ProductGrid(
+      productList: vm.cartList,
+      scroll: true,
+      onPressed: (productModel) {
+        vm.removeProduct(productModel);
+      },
+    );
+  }
+}
